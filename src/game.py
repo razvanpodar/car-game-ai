00c8fc43241ba7505174b35db3d430dfbf9aa3a8
razvanpodar <razvan_podar@yahoo.com>
2022-02-12 16:30:58 +0200

    Start implementing game logic

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
 import pygame
+from player import Player
 
 class Game:
 
     WINDOW_NAME = "Dodgecar"
     WIDTH = 480
-    HEGHT = 640
+    HEIGHT = 640
     FRAME_RATE = 60
     LANE_COUNT = 3
 
@@ -12,21 +13,27 @@ class Game:
         self.lanes = [0] * self.LANE_COUNT
         self.lane_width = self.WIDTH / self.LANE_COUNT
         self.running = True
+        self.score = 0
         
         for i, lane in enumerate(self.lanes):
             self.lanes[i] = self.lane_width * i
 
         pygame.init()
-        self.window = pygame.display.set_mode((self.WIDTH, self.HEGHT))
+        self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption(self.WINDOW_NAME)
+
+        self.player = Player(self.lanes[1], self.HEIGHT - 100, 1)
 
     def run(self):
         while (self.running):
-            self.window.fill(0)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+            
+            self.window.fill(0)
+            self.window.blit(self.player.sprite, self.player.pos)
+            pygame.display.flip()
+            
 
 if __name__ == "__main__":
     game = Game()
--- src/player.py
+++ src/player.py
@@ -1,4 +1,14 @@
+import pygame
+
 class Player:
     
-    def __init__(self):
-        pass
+    SPRITE_PATH = "../res/sprites/player.png"
+
+    def __init__(self, x, y, lane):
+        self.pos = (x, y)
+        self.lane = lane
+        self.sprite = pygame.image.load(self.SPRITE_PATH)
+
+    def move(self, x, y, lane):
+        self.pos = (x, y)
+        self.lane = lane
